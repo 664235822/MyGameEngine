ada61875ff7179b20cc2bd55fa3d406e097b3cb7
smallpig1997 <664235822@qq.com>
2023-02-03 21:58:12 +0800

    引擎框架搭建

---
+++ Sandbox/Program.cs
@@ -0,0 +1,10 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Sandbox;
+
+using (Window window = new Window(800, 600, "MyGameEngine"))
+{
+    //Run takes a double, which is how many frames per second it should strive to reach.
+    //You can leave that out and it'll just update as fast as the hardware will allow it.
+    window.RenderFrequency = 60;
+    window.Run();
+}
---
+++ Sandbox/Window.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+
+namespace Sandbox
+{
+    internal class Window : GameWindow
+    {
+        public Window(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            KeyboardState input = KeyboardState;
+
+            if (input.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
