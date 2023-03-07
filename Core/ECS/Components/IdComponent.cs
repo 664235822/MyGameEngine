72d54b7448598e38e5ffdd4ddeb8b7934e306f68
smallpig1997 <664235822@qq.com>
2023-03-07 14:46:28 +0800

    Id组件

---
+++ Core/ECS/Components/IdComponent.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.ECS.Components;
+
+public class IdComponent:IComponent
+{
+    public Guid Id { get; }
+
+    public IdComponent(Guid id)
+    {
+        Id = Guid.NewGuid();
+    }
+}
