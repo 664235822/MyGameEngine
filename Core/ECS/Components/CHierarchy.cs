5838efa43cc933b8f7c20059c16fdb0ecf8dc28a
smallpig1997 <664235822@qq.com>
2023-03-08 16:14:43 +0800

    Hierarchy层级组件

---
+++ Core/ECS/Components/CHierarchy.cs
@@ -0,0 +1,17 @@
+﻿namespace Core.ECS.Components;
+
+public class CHierarchy : IComponent
+{
+    public Guid Id { get; }
+
+    public Guid? ParentId { get; internal set; }
+
+    public List<Guid> Children { get; }
+
+    public CHierarchy()
+    {
+        Id = Guid.NewGuid();
+        ParentId = null;
+        Children = new List<Guid>();
+    }
+}
